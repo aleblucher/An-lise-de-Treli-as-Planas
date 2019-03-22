7914fb3c8431d47ac4d740681aaa4421bed03783
GiuPassarelli <gb_passarelli@hotmail.com>
2019-03-22 10:34:56 -0300

    Big Matrix Done

--- first_test.py
+++ first_test.py
@@ -9,24 +9,42 @@ def nos_sistema(quantidade):
         list.append([x,y])
     return list
 
-a = nos_sistema(2)
+a = nos_sistema(3)
 print(a)
 
 def elemento(lista_de_nos, no1, no2):
-    x1 = lista_de_nos[no1][0]
-    y1 = lista_de_nos[no1][1]
-    x2 = lista_de_nos[no2][0]
-    y2 = lista_de_nos[no2][1]
+    x1 = lista_de_nos[no1 - 1][0]
+    y1 = lista_de_nos[no1 - 1][1]
+    x2 = lista_de_nos[no2 - 1][0]
+    y2 = lista_de_nos[no2 - 1][1]
     dist = (x2 - x1)**2 + (y2-y1)**2
     cos = (x2 - x1)/np.sqrt(dist)
     sen = (y2 - y1)/np.sqrt(dist)
     mak = np.matrix([[cos**2, cos*sen, -(cos**2), -(cos*sen)],[cos*sen, sen**2, -(cos*sen), -(sen**2)],[-(cos**2), -(cos*sen), cos**2, cos*sen],[-(cos*sen), -(sen**2), cos*sen, sen**2]])
     return [mak, no1, no2]
 
-
-
 maks = []
 
-maks.append(elemento(a,0,1))
+maks.append(elemento(a,1,2))
+maks.append(elemento(a,2,3))
 
-def grau_liberdade_elemento(lista_de_nos, no1, no2):
+def make_Kg(maks, n_nos):
+    big_matrix = np.zeros((n_nos*2, n_nos*2))
+    for mak in maks:
+        first = (mak[1] * 2) - 1
+        second = mak[1] * 2
+        third = (mak[2] * 2) - 1
+        fourth = mak[2] * 2
+        for line in range(first, fourth + 1):
+            for column in range(first, fourth + 1):
+                big_matrix[line - 1][column - 1] += mak[0][(line - first, column - first)]
+
+    print("maks", maks)
+    #print("maks0", maks[0])
+    #print("maks00", maks[0][0][(0,0)])
+    print("big", big_matrix)
+
+    return big_matrix
+
+
+make_Kg(maks, 3)
